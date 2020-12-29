4e46ec3f8a45c0513c33c371fdd9fa6714205ecc
Mark Edward Ratio <markedwardratio@gmail.com>
2020-12-29 13:42:00 +0800

    Add dummy categoryController functions

---
+++ controllers/categoryController.js
@@ -0,0 +1,33 @@
+var Category = require('../models/category');
+
+exports.list = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category list');
+};
+
+exports.createGet = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category create GET');
+};
+
+exports.createPost = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category create POST');
+};
+
+exports.detail = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category detail: ' + req.params.id);
+};
+
+exports.updateGet = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category update GET');
+};
+
+exports.updatePost = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category update POST');
+};
+
+exports.deleteGet = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category delete GET');
+};
+
+exports.deletePost = function (req, res) {
+	res.send('NOT IMPLEMENTED: Category delete POST');
+};
