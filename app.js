59e467b0393331ac266eda5e661a5374eca67144
Mark Edward Ratio <markedwardratio@gmail.com>
2020-12-29 13:53:20 +0800

    Update app.js to use category and item router

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
+var categoryRouter = require('./routes/category');
+var itemRouter = require('./routes/item');
 
 var app = express();
 
@@ -28,6 +30,8 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/categories', categoryRouter);
+app.use('/items', itemRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
