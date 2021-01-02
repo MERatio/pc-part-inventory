3bde479cbc362e99e87eb79933874a7e7fb26ec2
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-02 12:50:47 +0800

    Add code for category controller detail

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,4 +1,6 @@
 const Category = require('../models/category');
+const Item = require('../models/item');
+const async = require('async');
 const { body, validationResult } = require('express-validator');
 
 exports.list = (req, res) => {
@@ -50,8 +52,36 @@ exports.createPost = [
 	},
 ];
 
-exports.detail = (req, res) => {
-	res.send('NOT IMPLEMENTED: Category detail: ' + req.params.id);
+exports.detail = (req, res, next) => {
+	const categoryId = req.params.id;
+	async.parallel(
+		{
+			category(callback) {
+				Category.findById(categoryId).exec(callback);
+			},
+			categoryItems(callback) {
+				Item.find({ category: categoryId }, 'name price stock').exec(callback);
+			},
+		},
+		(err, results) => {
+			if (err) {
+				next(err);
+			} else if (results.category === null) {
+				const err = new Error('Category not found');
+				err.status = 404;
+				next(err);
+			} else {
+				// Success, so render.
+				const categoryName = results.category.name;
+				const { category, categoryItems } = results;
+				res.render('categories/detail', {
+					title: `${categoryName} Detail`,
+					category,
+					categoryItems,
+				});
+			}
+		}
+	);
 };
 
 exports.updateGet = (req, res) => {
