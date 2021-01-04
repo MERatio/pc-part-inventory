12a240d136e7a4a3dbb02e338c38b0c9230f7967
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-04 14:22:45 +0800

    Add code for item controller createPost

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,5 +1,7 @@
 const Item = require('../models/item');
 const Category = require('../models/category');
+const async = require('async');
+const { body, validationResult } = require('express-validator');
 
 exports.list = (req, res, next) => {
 	Item.find({}, 'name category price stock')
@@ -29,9 +31,59 @@ exports.createGet = (req, res, next) => {
 		});
 };
 
-exports.createPost = (req, res) => {
-	res.send('NOT IMPLEMENTED: Item create POST');
-};
+exports.createPost = [
+	// Validate and sanitise fields.
+	body('name')
+		.trim()
+		.isLength({ min: 1, max: 100 })
+		.withMessage('Name must be 1 to 100 characters long')
+		.escape(),
+	body('description')
+		.trim()
+		.isLength({ min: 10, max: 500 })
+		.withMessage('Description must be 10 to 500 characters long')
+		.escape(),
+	body('category')
+		.isLength({ min: 1 })
+		.withMessage('Category must not be empty')
+		.escape(),
+	body('price')
+		.trim()
+		.isFloat({ min: 0, max: 999999 })
+		.withMessage('Price must be 0 to 999999 dollars')
+		.escape(),
+	body('stock')
+		.trim()
+		.isInt({ min: 0, max: 999999 })
+		.withMessage('Stock must be 0 to 999999')
+		.escape(),
+	// Process request after validation and sanitization.
+	(req, res, next) => {
+		// Extract the validation errors from a request.
+		const errors = validationResult(req);
+		// Create an Item object with escaped and trimmed data.
+		const item = new Item({
+			name: req.body.name,
+			description: req.body.description,
+			category: req.body.category,
+			price: req.body.price,
+			stock: req.body.stock,
+		});
+		if (!errors.isEmpty()) {
+			// There are errors. Render form again with sanitized values and error messages.
+			res.render('items/form', {
+				title: 'Create Item',
+				item,
+				errors: errors.array(),
+			});
+		} else {
+			// Data form is valid.
+			item.save((err) => {
+				err ? next(err) : res.redirect(item.url);
+			});
+		}
+	},
+];
 
 exports.detail = (req, res, next) => {
 	Item.findById(req.params.id)
