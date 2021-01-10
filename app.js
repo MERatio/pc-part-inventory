518bdd85d833626eb3742b0ec09fa77b51291518
Mark Edward Ratio <markedwardratio@gmail.com>
2021-01-10 08:31:13 +0800

    Add and use compression library

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const compression = require('compression');
 
 const indexRouter = require('./routes/index');
 const categoryRouter = require('./routes/category');
@@ -27,6 +28,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(compression()); // Compress all routes
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
